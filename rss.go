1697e50f2d70867817d38c348f4b1cdec65c9ba7
DanielRafel09 <44653947+DanielRafel09@users.noreply.github.com>
2025-04-25 10:54:23 -0600

    Update rss.go

--- rss.go
+++ rss.go
@@ -16,6 +16,7 @@ type RssItem struct {
 	feedName    string
 	published   *time.Time
 	description *string
+	status      string
 }
 
 type FeedDatabaseItem struct {
@@ -96,5 +97,6 @@ func convert(item *gofeed.Item, itemFeedName string) *RssItem {
 		feedName:    itemFeedName,
 		published:   item.PublishedParsed,
 		description: &item.Description,
+		status:      "Not started"
 	}
 }
